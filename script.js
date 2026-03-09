c685a5888ec03acce95db02f74f9daca349b9d82
elfishwax <felixshaw3112@gmail.com>
2026-03-09 13:07:34 +0800

    add semi-pseudocode for js + other small edits

---
+++ script.js
@@ -0,0 +1,22 @@
+/*let db = [1,1,0,1,1,1,0]
+
+
+
+
+for (let c = 0; c <= db.lenght; c++) {
+    if (db[c] === -1) {
+        document.getElementById(c).style.backgroundColor(red);
+    } 
+    else if (db[c] === 0) {
+        document.getElementById(c).style.backgroundColor(yellow);
+    }
+    else if (db[c] === 1) {
+        document.getElementById(c).style.backgroundColor(green);
+    }
+    else {
+        document.getElementById(c).style.backgroundColor(grey);
+    }
+}
+
+*/ 
+
