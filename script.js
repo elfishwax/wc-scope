e1f3a572831b5f3aebb694d86c939daa30eba591
elfishwax <felixshaw3112@gmail.com>
2026-04-04 19:12:09 +0800

    add *possibly working* javascript

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-/*let db = [1,1,0,1,1,1,0]
+let db = {cblockm: "1201", cblockf: "0210"}; // 0 = no door, 1 = no lock, 2 = fine
 
-
-
-
-for (let c = 0; c <= db.lenght; c++) {
-    if (db[c] === -1) {
-        document.getElementById(c).style.backgroundColor(red);
-    } 
-    else if (db[c] === 0) {
-        document.getElementById(c).style.backgroundColor(yellow);
-    }
-    else if (db[c] === 1) {
-        document.getElementById(c).style.backgroundColor(green);
-    }
-    else {
-        document.getElementById(c).style.backgroundColor(grey);
+function refreshStatus(block, gender){
+    let currentBlock = window[block + "block" + gender];
+    let currentNum
+    for (let i = 0; i++; i < db[currentBlock].length){ // figure out way to get length of db.var.length
+        let currentNum = db[currentBlock][i];   
+        if (currentNum == 0){
+            document.getElementById(i).style.backgroundColor("red")
+        }
+        else if (currentNum == 1){
+            document.getElementById(i).style.backgroundColor("orange")
+        }
+        else if (currentNum == 2){
+            document.getElementById(i).style.backgroundColor("green")
+        }
+        else {
+            document.getElementById(i).style.backgroundColor("gray")
+        }
     }
 }
-
-*/ 
-
